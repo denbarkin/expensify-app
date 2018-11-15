c0cbffb597aae83d278ff0405438adc6a226cfd8
Deniz Barkın Takmaz <denizbarkintakmaz@Denizs-MacBook-Pro.local>
2018-11-15 14:09:20 +0300

    ExpenseListItem format is corrected

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from 'react-router-dom';
+import numeral from 'numeral';
+import moment from 'moment';
 
 const ExpenseListItem = ({id, description, amount, createdAt}) => {
     return(
@@ -8,7 +10,11 @@ const ExpenseListItem = ({id, description, amount, createdAt}) => {
             <Link to={`Edit/${id}`} >
                 <h3>{description}</h3>
             </Link>
-            <p>{amount} - {createdAt}</p>
+            <p>
+                {numeral(amount).format('$0,0.00')} 
+                - 
+                {moment(createdAt).format('MMMM Do, YYYY')}
+            </p>
         </div>
     );
 };
